759eb11ad7aeb95275489f82c7f973ddcd7c0ab9
grant kinghorn <gkinghorn3@icloud.com>
2023-09-15 00:16:51 +0100

    m

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,20 @@
 import { useState } from "react";
 import Home from './routes/home/home.component';
+import { Routes, Route } from "react-router-dom";
+import Navigation from './routes/navigation/Navigation.component';
+
+
 
 const App = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<Navigation />} >
+        <Route index element={<Home />} />
+      </ Route>
+    </Routes>
+  )
 
-  return <Home />;
+  
 
 };
 
