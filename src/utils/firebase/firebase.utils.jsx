cdd90027a29cd965d85c5fae4922456d43bcfaf1
Grant <gkinghorn3@icloud.com>
2023-09-18 15:35:18 +0100

    firestore set up

--- src/utils/firebase/firebase.utils.jsx
+++ src/utils/firebase/firebase.utils.jsx
@@ -1,23 +1,35 @@
-import { initializeApp } from 'firebase/app';
-import { getAuth, signInWithRedirect, signInWithPopup, GoogleAuthProvider } from 'firebase/auth';
-
+import { initializeApp } from "firebase/app";
+import {
+  getAuth,
+  signInWithRedirect,
+  signInWithPopup,
+  GoogleAuthProvider,
+} from "firebase/auth";
+import { getFirestore, doc, getDoc, setDoc } from "firebase/firestore";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCq4cEQAD9S4aDB8EqG50vBr1BCNYI4SsE",
-    authDomain: "ecom-store-db-9c5d9.firebaseapp.com",
-    projectId: "ecom-store-db-9c5d9",
-    storageBucket: "ecom-store-db-9c5d9.appspot.com",
-    messagingSenderId: "282798152548",
-    appId: "1:282798152548:web:2523be77530b93c7723871"
-  };
-  
+  apiKey: "AIzaSyCq4cEQAD9S4aDB8EqG50vBr1BCNYI4SsE",
+  authDomain: "ecom-store-db-9c5d9.firebaseapp.com",
+  projectId: "ecom-store-db-9c5d9",
+  storageBucket: "ecom-store-db-9c5d9.appspot.com",
+  messagingSenderId: "282798152548",
+  appId: "1:282798152548:web:2523be77530b93c7723871",
+};
 
-  const FirebaseApp = initializeApp(firebaseConfig);
+const FirebaseApp = initializeApp(firebaseConfig);
 
-  const provider = new GoogleAuthProvider();
-  provider.setCustomParameters({
-    prompt: "select_account"
-  })
+const provider = new GoogleAuthProvider();
+provider.setCustomParameters({
+  prompt: "select_account",
+});
 
-  export const auth = getAuth();
-  export const signInWithGooglePopup = () => signInWithPopup(auth, provider)
+export const auth = getAuth();
+export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
+
+export const db = getFirestore();
+
+export const createUserDocumentFromAuth = async (userAuth) => {
+  const userDocRef = doc(db, "users", userAuth.uid);
+
+  console.log(userDocRef);
+};
