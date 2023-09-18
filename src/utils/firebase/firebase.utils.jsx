2668b4bd6567ce9eb9b4d7585ff2013c0d06788f
grant kinghorn <gkinghorn3@icloud.com>
2023-09-18 19:38:38 +0100

    function to create new user

--- src/utils/firebase/firebase.utils.jsx
+++ src/utils/firebase/firebase.utils.jsx
@@ -34,6 +34,23 @@ export const createUserDocumentFromAuth = async (userAuth) => {
   console.log(userDocRef);
 
   const userSnapshot = await getDoc(userDocRef); 
-    console.log(userSnapshot);
-    console.log(userSnapshot.exists());
-};
+
+  
+    if (!userSnapshot.exists()) {
+      const { displayName, email } = userAuth;
+      const createdAt = new Date();
+    
+      try {
+        await setDoc(userDocRef, {
+          displayName,
+          email,
+          createdAt,
+        });
+      } catch (error) {
+        console.log("Error creating user", error.message);
+      }
+    
+      return userDocRef;
+    }
+  }    
+ 
